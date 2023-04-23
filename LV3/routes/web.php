dbf06e9763af4a9e8c51837afcfa72edd562242a
Marko Pavičić <mpavicic99@gmail.com>
2023-04-23 12:36:16 +0200

    LV3 - Routing fixes
    - allow only authenticated users to access app routes
    - set login page as root page
    - if unauthenticated users try to navigate to app routes they are redirected to the login page

--- LV3/routes/web.php
+++ LV3/routes/web.php
@@ -13,31 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',function(){
+    return view('auth.login');
+})->middleware(['auth']);
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Ruta za prikaz obrasca za otvaranje novog projekta
-Route::get('/projects/create', [App\Http\Controllers\ProjectController::class, 'create'])->name('projects.create');
-
-// Ruta za spremanje novog projekta
-Route::post('/projects', [App\Http\Controllers\ProjectController::class, 'store'])->name('projects.store');
-
-// Ruta za prikaz pojedinog projekta
-Route::get('/projects/{project}', [App\Http\Controllers\ProjectController::class, 'show'])->name('projects.show');
-
-// Ruta za prikaz obrasca za uređivanje pojedinog projekta
-Route::get('/projects/{project}/edit', [App\Http\Controllers\ProjectController::class, 'edit'])->name('projects.edit');
-
-// Ruta za ažuriranje pojedinog projekta
-Route::put('/projects/{project}', [App\Http\Controllers\ProjectController::class, 'update'])->name('projects.update');
-
-// Ruta za brisanje pojedinog projekta
-Route::delete('/projects/{project}', [App\Http\Controllers\ProjectController::class, 'destroy'])->name('projects.destroy');
-
-// Ruta za prikaz svih projekata za prijavljenog korisnika
-Route::get('/projects', [App\Http\Controllers\ProjectController::class, 'index'])->name('projects.index');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+    // Ruta za prikaz obrasca za otvaranje novog projekta
+    Route::get('/projects/create', [App\Http\Controllers\ProjectController::class, 'create'])->name('projects.create');
+    
+    // Ruta za spremanje novog projekta
+    Route::post('/projects', [App\Http\Controllers\ProjectController::class, 'store'])->name('projects.store');
+    
+    // Ruta za prikaz pojedinog projekta
+    Route::get('/projects/{project}', [App\Http\Controllers\ProjectController::class, 'show'])->name('projects.show');
+    
+    // Ruta za prikaz obrasca za uređivanje pojedinog projekta
+    Route::get('/projects/{project}/edit', [App\Http\Controllers\ProjectController::class, 'edit'])->name('projects.edit');
+    
+    // Ruta za ažuriranje pojedinog projekta
+    Route::put('/projects/{project}', [App\Http\Controllers\ProjectController::class, 'update'])->name('projects.update');
+    
+    // Ruta za brisanje pojedinog projekta
+    Route::delete('/projects/{project}', [App\Http\Controllers\ProjectController::class, 'destroy'])->name('projects.destroy');
+    
+    // Ruta za prikaz svih projekata za prijavljenog korisnika
+    Route::get('/projects', [App\Http\Controllers\ProjectController::class, 'index'])->name('projects.index');
+ });
+ 
